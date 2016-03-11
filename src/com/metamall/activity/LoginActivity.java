947aeed001206b1bfe6259e0a3fd4d7ad5b28a1e
MorningBells <85073439@qq.com>
2016-03-11 21:14:21 +0800

    代码上传

--- src/com/metamall/activity/LoginActivity.java
+++ src/com/metamall/activity/LoginActivity.java
@@ -1,175 +1,107 @@
 package com.metamall.activity;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.text.method.HideReturnsTransformationMethod;
-import android.text.method.PasswordTransformationMethod;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import com.metamall.R;
-import com.metamall.fragment.HomeFragment;
-import com.metamall.utils.HttpUtil;
-import com.metamall.utils.RemoteServiceEnum;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.text.Editable;
+        import android.text.TextWatcher;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ImageButton;
+        import com.metamall.R;
 
+        /**
+ + * 注册和登录Activity
+ + *
+ + */
+        public class LoginActivity extends Activity {
 
-import java.util.HashMap;
-import java.util.Map;
+            	private ImageButton ibBack;
+    	private EditText etAccount;
+    	private EditText etPassword;
+    	private Button btnLogin;
+    	private Button btnRegister;
 
-/**
- * 注册和登录Activity
- *
- */
-public class LoginActivity extends Activity {
+           	@Override
+   	protected void onCreate(Bundle savedInstanceState) {
+        		super.onCreate(savedInstanceState);
+      		setContentView(R.layout.activity_login);
 
-	private ImageButton ibBack;
-	private EditText username;
-	private EditText password=null;
-    private CheckBox check=null;
-	private Button btnLogin;
-	private Button btnRegister;
-    private String password1;
-    private String password3;
-    private String username3;
-    private String username1;
+                		initView();
+        	}
 
+            	/**
+     +	 * 初始化视图
+     +	 */
+            	private void initView() {
+        		ibBack = (ImageButton) findViewById(R.id.login_ib_back);
+        		ibBack.setOnClickListener(new OnClickListener() {
 
+                    			@Override
+            			public void onClick(View v) {
+                				setResult(RESULT_CANCELED);
+                				finish();
+                			}
+            		});
+        		MyWatcher watcher = new MyWatcher();
+        		etAccount = (EditText) findViewById(R.id.login_et_account);
+        		etAccount.addTextChangedListener(watcher);
+        		etPassword = (EditText) findViewById(R.id.login_et_password);
+        		etPassword.addTextChangedListener(watcher);
+        		btnLogin = (Button) findViewById(R.id.login_btn_login);
+        		btnLogin.setOnClickListener(new OnClickListener() {
+            			@Override
+            			public void onClick(View v) {
+                                //TODO:登陆操作
+                                        String accountNo = etAccount.getText().toString();
+                                String password = etPassword.getText().toString();
+                                //去后台验证
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.activity_login);
-        initView();
-        this.password=(EditText)super.findViewById(R.id.password);
-        this.check=(CheckBox)super.findViewById(R.id.check);
-        Bundle bundle1=this.getIntent().getExtras();
-        if(bundle1!= null){
-            password3=bundle1.getString("password2");
-            username3=bundle1.getString("username2");
-        }
-        //为check设置监听选项，控制密码框的显示方式
-        this.check.setOnClickListener(new OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                if(check.isChecked())
-                {
-                        //设置密码可见
-                        password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-                    }
-                    else
-                    {
-                        //设置密码隐藏
-                        password.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                    }
-                }
-            });
-        }
+                                                if( 1==1 ){
 
+                                            }else{
+                                        //登陆失败
 
-	/**
-	 * 初始化视图
-	 */
-	private void initView() {
-        try {
-            ibBack = (ImageButton) findViewById(R.id.login_ib_back);
-            ibBack.setOnClickListener(new OnClickListener() {
+                                                    }
 
-                @Override
-                public void onClick(View v) {
-                    setResult(RESULT_CANCELED);
-                    finish();
-                }
-            });
-            MyWatcher watcher = new MyWatcher();
-            username = (EditText) findViewById(R.id.login_et_account);
-            username.addTextChangedListener(watcher);
-            password = (EditText) findViewById(R.id.login_et_password);
-            password.addTextChangedListener(watcher);
-            btnLogin = (Button) findViewById(R.id.login_btn_login);
-            btnLogin.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //登陆操作
-                    String accountNo = username.getText().toString();
-                    String password1 = password.getText().toString();
-                    //远程调用登陆服务
-                    Map<String, String> map = new HashMap<String, String>();
-                    map.put("accountNo", accountNo);
-                    map.put("password1", password1);
-                    //返回json格式数据类型
-                    String response = HttpUtil.post(RemoteServiceEnum.LOGIN, map);
+                                    }
+            		});
+        		btnRegister = (Button) findViewById(R.id.login_btn_register);
+        		btnRegister.setOnClickListener(new OnClickListener() {
+            			@Override
+            			public void onClick(View v) {
+                                //TODO:跳转注册界面
 
-                    if (password1.equals("password2")) {
-                        Intent intent =
-                                new Intent(LoginActivity.this, HomeFragment.class);       //意图跳到主页
-                        startActivity(intent);
+                                			}
+            		});
+        	}
 
-                    } else {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                        builder.setTitle("警告");
-                        builder.setMessage("账号密码不匹配");
-                        builder.setPositiveButton("确定", null);
-                        builder.show();
-                        return;
-                        //登陆失败
+            	/**
+     +	 * 监听文本框
+     +	 */
+            	class MyWatcher implements TextWatcher {
 
-                    }
+                		@Override
+        		public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+                                           				int arg3) {
+            			if ("".equals(etAccount.getText().toString())
+                    					|| "".equals(etPassword.getText().toString())) {
+                				btnLogin.setEnabled(false);
+                				btnRegister.setEnabled(false);
+                			} else {
+                				btnLogin.setEnabled(true);
+                				btnRegister.setEnabled(true);
+                			}
+            		}
 
-                }
-            });
-            btnRegister = (Button) findViewById(R.id.login_btn_register1);
-            btnRegister.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //TODO:跳转注册界面
-                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                    startActivity(intent);
+                		@Override
+        		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+                                               				int arg3) {
+            		}
 
-
-                }
-            });
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-	}
-
-	/**
-	 * 监听文本框
-	 */
-	class MyWatcher implements TextWatcher {
-
-		@Override
-		public void onTextChanged(CharSequence arg0, int arg1, int arg2,
-				int arg3) {
-			if ("".equals(username.getText().toString())
-					|| "".equals(password.getText().toString())) {
-				btnLogin.setEnabled(false);
-				btnRegister.setEnabled(false);
-			} else {
-				btnLogin.setEnabled(true);
-				btnRegister.setEnabled(true);
-			}
-		}
-
-		@Override
-		public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
-				int arg3) {
-		}
-
-		@Override
-		public void afterTextChanged(Editable arg0) {
-		}
-
-	}
-
-}
+                		@Override
+        		public void afterTextChanged(Editable arg0) {
+            		}
+        	}
+    }
--- src/com/metamall/activity/RegisterActivity.java
+++ src/com/metamall/activity/RegisterActivity.java
@@ -1,14 +1,7 @@
 package com.metamall.activity;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import com.metamall.R;
 
+import android.app.Activity;
 
 /**
  * <p>.</p>
@@ -17,78 +10,5 @@ import com.metamall.R;
  * @version RegisterActivity.java 1.0 Created@2015-11-14 20:43 $
  */
 public class RegisterActivity extends Activity {
-    private EditText username;
-    private EditText password=null;
-    private EditText repassword=null;
-    private Button register;
-    private Button cancel;
-    public String id;
-    public String password2=password.getText().toString();
-    String repassword2=repassword.getText().toString();
-
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        setTitle("注册");
-        username = (EditText) findViewById(R.id.userName);
-        password = (EditText) findViewById(R.id.password);
-        repassword = (EditText) findViewById(R.id.repassword);
-        Button register = (Button) findViewById(R.id.register);
-        Button cancel = (Button) findViewById(R.id.cancel);
-    }
-
-
-    public void register_Click(View view) {
-
-
-        if (username.getText().toString().equals("")) {
-            new AlertDialog.Builder(this)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle("警告")
-                    .setMessage("请输入用户名")
-                    .setPositiveButton("确定", null)
-                    .show();
-            return;
-        }
-        if (!password2.equals(repassword2)) {
-            new AlertDialog.Builder(this)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle("警告")
-                    .setMessage("两次密码输入不相同")
-                    .setPositiveButton("确定", null)
-                    .show();
-            return;
-        }
-
-
-
-
-
-        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-
-        Bundle bundle1=new Bundle();
-        bundle1.putString("password2", password.getText().toString());
-        bundle1.putString("username2",username.getText().toString());
-        intent.putExtras(bundle1);
-        startActivity(intent);             //启动这个activity
-        this.finish();                   //结束本个Activity
-    }
-
-    public void cancel_Click(View view){
-        finish();
-    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
